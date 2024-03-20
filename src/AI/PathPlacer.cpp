d2db1685530284e204534760a80c7660b8d1fe5c
samphillippo <samphillippo1@gmail.com>
2024-03-20 16:21:13 -0400

    develops pathing algorithm for pathplacer

--- src/AI/PathPlacer.cpp
+++ src/AI/PathPlacer.cpp
@@ -7,8 +7,8 @@ PathPlacer::PathPlacer(glm::vec3 center, float sideLength) : SentientCube(center
     m_movementTicks = 30;
     m_minActionTicks = 40;
     m_avgActionTicks = 40;
-    m_maxPathRange = 10;
-    m_minPathRange = 5;
+    m_maxPathRange = 8;
+    m_minPathRange = 1;
 }
 
 PathPlacer::~PathPlacer() {
@@ -21,13 +21,39 @@ glm::vec3 PathPlacer::GetRandomVector() {
     return glm::vec3(x, y, z);
 }
 
+//Pathfinds, following optimal route along randomly chosen vector
 void PathPlacer::PlanPath() {
     glm::vec3 randomVector = GetRandomVector();
-    std::cout << "randomVector: " << randomVector.x << " " << randomVector.y << " " << randomVector.z << std::endl;
     m_path.clear();
-    m_path.push_back(glm::vec3(2, 2, 2));
-    m_path.push_back(glm::vec3(3, 3, 3));
-    //forces next tick to be a move tick
+
+    //Total distance needed to travel in each direction
+    glm::vec3 delta = abs(randomVector);
+    int sx = (0 <= randomVector.x) ? 1 : -1;
+    int sy = (0 <= randomVector.y) ? 1 : -1;
+    int sz = (0 <= randomVector.z) ? 1 : -1;
+
+    int numSteps = delta.x + delta.y + delta.z;
+    glm::vec3 slope = glm::vec3(delta.x/ numSteps, delta.y/ numSteps, delta.z/ numSteps);
+    glm::vec3 stepTracker = slope;
+
+    glm::vec3 currentLocation = m_center;
+    glm::vec3 finalLocation = m_center + randomVector;
+
+    for (int i = 0; i < numSteps; i++) {
+        if (stepTracker.x >= stepTracker.y && stepTracker.x >= stepTracker.z) {
+            currentLocation.x += sx;
+            stepTracker.x -= 1;
+        } else if (stepTracker.y >= stepTracker.z) {
+            currentLocation.y += sy;
+            stepTracker.y -= 1;
+        } else {
+            currentLocation.z += sz;
+            stepTracker.z -= 1;
+        }
+        stepTracker += slope;
+
+        m_path.push_back(currentLocation);
+    }
     m_tickCount = m_movementTicks - 1;
 }
 
